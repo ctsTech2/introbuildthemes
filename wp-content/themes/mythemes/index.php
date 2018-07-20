327ce456db9875623389a538fb1ccb18876dd92d
Ryan Templin <tempjuice@hotmail.com>
2018-07-19 19:21:31 -0500

    Completed basic theme.

--- wp-content/themes/mythemes/index.php
+++ wp-content/themes/mythemes/index.php
@@ -1,13 +1,18 @@
 <meta http-equiv="refresh" content="3">
 
- 
-<?php if( have_posts() ) : ?>
-	<?php while( have_posts() ) : the_post() ?>
-		<h2><a href='<?php the_permalink() ?>'><?php the_title() ?></a></h2>
-		<div class="content">
-			<?php the_content() ?>
-		</div>
-	<?php endwhile ?>
-<?php else : ?>
-	<p>Oh No, there are no posts!</p>
-<?php endif ?>
+ <?php get_header() ?>
+
+<?php 
+	if( have_posts() ) {
+		while( have_posts() ) { 
+			the_post();
+			get_template_part( 'template-parts/content', '' );
+		}
+	}
+	else {
+		get_template_part( 'template-parts/content', 'none' );
+	}
+?>
+
+
+<?php get_footer() ?>
